a200abd3d7b165a13663a9e792534eaa8f2243e8
addisonnitto24 <ajr7wt@virginia.edu>
2025-03-21 10:31:21 -0400

    Final push of booklover package and notebook for Module 9 HW

---
+++ lessons/M09/M09-demo/.ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="demo",
+    version="0.1",
+    description="A simple demonstration package",
+    url="https://github.com/addisonnitto24/M09-demo",
+    author="R.C. Alvarado",
+    author_email="ontoligent@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[],
+)
+
+
